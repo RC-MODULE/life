b595b6d67193b6c3a3c0d1726256d1bea3d258a0
Ivan Zhilenkov <i.zhilenkov@module.ru>
2019-12-21 15:33:31 +0300

    step 10 vs2005 -> vs2015

--- life_010/make_mc7601/premake5.lua
+++ life_010/make_mc7601/premake5.lua
@@ -3,15 +3,16 @@
 -- A solution contains projects, and defines the available configurations
 solution "life-mc7601"
    configurations { "Debug", "Release" }
+   startproject "life-mc7601-x86"
 
    -- A project defines one build target
    project "life-mc7601-x86"
       kind "ConsoleApp"
       language "C++"
       files { "**.h", "../src_pc/*.cpp"}
-	  libdirs {   "$(MC7601)/libload", "$(HAL)/lib", "$(VSHELL32)/lib", "$(NMPP)/lib" }
-	  includedirs { "$(MC7601)/include", "$(HAL)/include", "$(VSHELL32)/include", "$(NMPP)/include"  }
-	  links { "mc7601_ll_pc.lib","hal-mc7601-x86.lib", "vshell32.lib", "nmpp-x86.lib", "life-mc7601-nmc3-0","life-mc7601-nmc3-1" }
+	  libdirs {   "$(MC7601)/libload", "$(HAL)/lib", "$(VSHELL)/lib", "$(NMPP)/lib" }
+	  includedirs { "$(MC7601)/include", "$(HAL)/include", "$(VSHELL)/include", "$(NMPP)/include"  }
+	  links { "mc7601_ll_pc.lib","hal-mc7601-x86d.lib", "vshell.lib", "nmpp-x86.lib", "life-mc7601-nmc3-0","life-mc7601-nmc3-1" }
 
       configuration "Debug"
          defines { "DEBUG" }
